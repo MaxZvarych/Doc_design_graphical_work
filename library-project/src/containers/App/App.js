62a28b089110b3b56559df17d0c4105faad7b00b
Maksym-Fedir Zvarych <sofia.didula@gmail.com>
2022-06-05 14:00:07 +0300

    Added navigation, moved all global components and pages wrapped with navigation to App.js

--- library-project/src/containers/App/App.js
+++ library-project/src/containers/App/App.js
@@ -1,10 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import Header from "./Header/Header";
+import Navigation from "../Navigation/Navigation";
+import Footer from "./Footer/Footer";
+import { Provider } from "react-redux";
+import store from "../utils/Redux/Store";
+import SignUp from "../Authorization/SignUp/SignUp";
 
 function App() {
+  let myStorage = window.localStorage;
   return (
     <div className="App">
-      
+      <Header />
+      <Provider store={store}>
+        <Navigation></Navigation>
+      </Provider>
+      <Footer />
     </div>
   );
 }
