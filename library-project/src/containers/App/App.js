68d974f6e530bcca2d5ac112487243798a48e0b1
Maksym-Fedir Zvarych <sofia.didula@gmail.com>
2022-06-05 14:02:24 +0300

    Implemented cardItem component for showing items in catalog

--- library-project/src/containers/App/App.js
+++ library-project/src/containers/App/App.js
@@ -1,14 +1,12 @@
 import "./App.css";
-import React, { useState } from "react";
+import React from "react";
 import Header from "./Header/Header";
 import Navigation from "../Navigation/Navigation";
 import Footer from "./Footer/Footer";
 import { Provider } from "react-redux";
 import store from "../utils/Redux/Store";
-import SignUp from "../Authorization/SignUp/SignUp";
 
 function App() {
-  let myStorage = window.localStorage;
   return (
     <div className="App">
       <Header />
